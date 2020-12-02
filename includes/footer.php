766277bb129b8ea9906b0b42113323f8915fba23
Hansenlobo <Hansenlobo07@gmail.com>
2020-12-02 22:31:58 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,14 +1,10 @@
 <!-- Footer -->
 <footer>
 <div class="w3-center w3-small w3-opacity">
-W3Schools is optimized for learning, testing, and training. Examples might be simplified to improve reading and basic understanding.
-Tutorials, references, and examples are constantly reviewed to avoid errors, but we cannot warrant full correctness of all content.
-While using this site, you agree to have read and accepted our <a href="about_copyright.html">terms of use</a>,
-<a href="about_privacy.html">cookie and privacy policy</a>.
-<a href="about_copyright.html">Copyright 1999-<?php echo date('Y'); ?></a> by Refsnes Data. All Rights Reserved.<br>
- <a href="index.php">Powered by Abir</a>.<br><br>
-<a href="../index.html">
-<img style="width:150px;height:28px;border:0" src="../images/w3schoolscom_gray.gif" alt="Simple CMS"></a>
+
+<a href="about_copyright.html">Copyright 2020</a>. All Rights Reserved.<br>
+ <a href="index.php">Home</a>.<br><br>
+
 </div>
 <!-- jQuery -->
 <script src="js/jquery.js"></script>
